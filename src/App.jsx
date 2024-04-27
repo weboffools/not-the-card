ea99a0b199ccc8aa23bdbe1b8990a1151e75e12a
Eliot Smith <eliot@weboffools.com>
2024-04-27 17:01:22 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="container">
+      <Board />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Board.jsx
@@ -0,0 +1,9 @@
+const numCards = 12;
+
+export default function Board() {
+  return (
+    <div className="board">
+      for (let i = 0; i < numCards; i++)
+    </div>
+  )
+}
