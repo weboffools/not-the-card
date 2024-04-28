ebab626ac75414ba647f9224835569c9bb351dcd
Eliot Smith <eliot@weboffools.com>
2024-04-28 09:22:56 -0600

    Add fetching and 10 cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,21 @@
-import '../styles/Card.css'
+import "../styles/Card.css";
+import { useEffect } from "react";
+const url = 'https://akabab.github.io/starwars-api/api/id/';
 
-export default function Card() {
-  return (
-    <div className="card"><p>CARD</p></div>
-  );
+export default function Card({ index, id, cards, addCard }) {
+  useEffect(
+    () => {
+    let ignore = false;
+    fetch(`${url}${id}.json`)
+        .then(response => response.json())
+        .then((response) => {
+      if (!ignore) {
+        addCard(r => [...r, response.image]);
+      }
+    });
+    return () => {
+      ignore = true;
+    }
+  }, [addCard, id ]);
+  return <div className="card"><img src={cards[Number(index)]} alt=''/></div>;
 }
--- src/components/CardArea.jsx
+++ src/components/CardArea.jsx
@@ -1,15 +1,22 @@
-import '../styles/CardArea.css';
-import Card from './Card.jsx';
+import "../styles/CardArea.css";
+import Card from "./Card.jsx";
+import { useState } from "react";
 
 export default function CardArea() {
-  let cards = [];
-  for (let i = 0; i < 10; i++) {
-    cards.push(<Card key={i} />);
-  }
+  const [cards, setCards] = useState("");
+
   return (
     <div className="card-area">
-    {cards}
-
+      <Card index='0' addCard={setCards} cards={cards} id="1" />
+      <Card index='1' addCard={setCards} cards={cards} id="2" />
+      <Card index='2' addCard={setCards} cards={cards} id="3" />
+      <Card index='3' addCard={setCards} cards={cards} id="4" />
+      <Card index='4' addCard={setCards} cards={cards} id="5" />
+      <Card index='5' addCard={setCards} cards={cards} id="6" />
+      <Card index='6' addCard={setCards} cards={cards} id="7" />
+      <Card index='7' addCard={setCards} cards={cards} id="8" />
+      <Card index='8' addCard={setCards} cards={cards} id="9" />
+      <Card index='9' addCard={setCards} cards={cards} id="10" />
     </div>
   );
 }
