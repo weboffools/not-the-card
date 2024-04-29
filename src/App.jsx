4cba5644894631671d586ef97f8244cbab3b36cc
Eliot Smith <eliot@weboffools.com>
2024-04-29 16:16:18 -0600

    Update score state and add to handleClick for Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-
-import Sidebar from './components/Sidebar';
-import CardArea from './components/CardArea';
+import Sidebar from "./components/Sidebar";
+import CardArea from "./components/CardArea";
+import { useState } from "react";
 
 function App({ title, subtitle }) {
+  const [score, setScore] = useState(0);
   return (
-    <div className='container'>
-    <div className='header'>
-      <h1>{title}</h1>
-      <h2>{subtitle}</h2>
+    <div className="container">
+      <div className="header">
+        <h1>{title}</h1>
+        <h2>{subtitle}</h2>
+      </div>
+      <Sidebar score={score} />
+      <CardArea score={score} updateScore={setScore} />
     </div>
-      <Sidebar />
-      <CardArea />
-    </div>
-  )
+  );
 }
 
 export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,24 @@
 import "../styles/Card.css";
+import { useState } from "react";
 
-export default function Card({ pic, name, updateScore }) {
+export default function Card({ pic, name, updateScore, score }) {
+  const [clicked, setClicked] = useState(false);
 
-  return <div className='card' onClick={() => updateScore(score => score + 1)}><h3>{name}</h3><img src={pic} alt={name}/></div>;
+  function handleClick() {
+    if (clicked) {
+      alert(`You already clicked that card! Your final score is ${score}`);
+      updateScore(0);
+      setClicked(false);
+    } else {
+      updateScore((score) => score + 1);
+      setClicked(true);
+    }
+  }
+
+  return (
+    <div className="card" onClick={handleClick}>
+      <h3>{name}</h3>
+      <img src={pic} alt={name} />
+    </div>
+  );
 }
